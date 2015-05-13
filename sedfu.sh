b5921f1548157c51c02efb0d443a9778810058bb
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2015-05-13 15:28:35 +0200

    Fixed error in the script and affected file

--- sedfu.sh
+++ sedfu.sh
@@ -4,7 +4,7 @@ file=$1
 dos2unix $file
 dos2unix -c mac $file
 
-grep -vE '     ' field_statss |
+grep -vE '     ' $file |
     sed -e 's/   //g' |
     grep -vE '",|Hex|ID|Stage|tree|\{|\]|\[|ground|-|TEX|MC|MT|TT|MS|SP|GI|#|DH|BE|^$|\}|TI|AM|NN|Parts|SH|HS|BC|WA|TO|/|Rnd|TR|0"|1"|2"|3"|4"|5"|6"|7"|8"|9"|RE|MI|PU|GG|FG|%%|""|" |AA|=|VV' |
     sed -e 's/"//g' |
